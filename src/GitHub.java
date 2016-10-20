7802f98eb68767703d6d4f9666fd47900827e3bf
leiyunjian <378159335@qq.com>
2016-10-20 10:45:10 +0800

    branchone commit second

--- src/GitHub.java
+++ src/GitHub.java
@@ -5,5 +5,6 @@ public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("init master");
 		System.out.println("is branchone");
+		System.out.println("commit second");
 	}
 }
