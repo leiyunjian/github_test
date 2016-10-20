68e178bd27686703a2777707e238a5cc1fc37e1f
leiyunjian <378159335@qq.com>
2016-10-20 10:30:30 +0800

    revert to init master

--- src/GitHub.java
+++ src/GitHub.java
@@ -1,17 +1,8 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * Created by Lei on 2016/10/19.
  */
 public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("init master");
-		System.out.println("commit master  second");
-		System.out.println("commit branchone first");
-		Date date = new Date();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println(format.format(date));
-		System.out.println("commit master thrid");
 	}
 }
